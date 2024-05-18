f361ea3baff74a929c9fc6b622bb5c63ee881429
Mark Barrie <mark.a.barrie@gmail.com>
2024-05-18 19:38:22 -0400

    updated resolver,auths in backend

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,40 +1,46 @@
 const { User } = require('../models');
 
-
 const resolvers = {
-    Query: {
-        users: async () => {
-            return await User.find({});
-        },
-        user: async (_, { _id }) => {
-            return await User.findById(_id);
-        },
-        bookByTitle: async (_, { title }) => {
-            // Implementation to search for a book by title, possibly querying an external API
-        },
+  Query: {
+    users: async () => {
+      return await User.find({});
     },
-    Mutation: {
-        addUser: async (_, { username, email, password }) => {
-            const user = new User({ username, email, password });
-            return await user.save();
-        },
-        saveBook: async (_, { userId, book }) => {
-            const user = await User.findById(userId);
-            if (!user) {
-                throw new Error('User not found');
-            }
-            user.savedBooks.push(book);
-            return await user.save();
-        },
-        removeBook: async (_, { userId, bookId }) => {
-            const user = await User.findById(userId);
-            if (!user) {
-                throw new Error('User not found');
-            }
-            user.savedBooks = user.savedBooks.filter(book => book.bookId !== bookId);
-            return await user.save();
-        },
+    user: async (_, { _id }) => {
+      return await User.findById(_id);
     },
+    bookByTitle: async (_, { title }) => {
+      // Placeholder for book search by title logic
+      throw new Error('Not implemented');
+    },
+  },
+  Mutation: {
+    addUser: async (_, { username, email, password }) => {
+      const user = new User({ username, email, password });
+      await user.save();
+      return user;
+    },
+    saveBook: async (_, { userId, book }) => {
+      const user = await User.findById(userId);
+      if (!user) {
+        throw new Error('User not found');
+      }
+      user.savedBooks.push(book);
+      await user.save();
+      return user;
+    },
+    removeBook: async (_, { userId, bookId }) => {
+      const user = await User.findById(userId);
+      if (!user) {
+        throw new Error('User not found');
+      }
+      user.savedBooks = user.savedBooks.filter(book => book.bookId !== bookId);
+      await user.save();
+      return user;
+    },
+  },
+  User: {
+    bookCount: (parent) => parent.savedBooks.length,
+  },
 };
 
 module.exports = resolvers;
